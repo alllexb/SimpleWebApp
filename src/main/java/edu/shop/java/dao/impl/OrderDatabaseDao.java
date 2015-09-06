4c8226c9c7b3a96636b9c68d9deeb03933341146
alllexb <allexb@list.ru>
2015-09-06 18:02:24 +0300

    Fourth steps in programming Internet Shop Configuring database connections and organizing Hibernate SessionFactory. Making first JSP view.

--- src/main/java/edu/shop/java/dao/impl/OrderDatabaseDao.java
+++ src/main/java/edu/shop/java/dao/impl/OrderDatabaseDao.java
@@ -1,11 +1,11 @@
 package edu.shop.java.dao.impl;
 
-import edu.shop.java.dao.OrderDAO;
+import edu.shop.java.dao.OrderDao;
 import edu.shop.java.models.Order;
 import org.springframework.stereotype.Repository;
 
 @Repository("orderDatabaseDao")
-public class OrderDatabaseDao extends HibernateAbstractDao<Order> implements OrderDAO{
+public class OrderDatabaseDao extends HibernateAbstractDao<Order> implements OrderDao {
 
     public OrderDatabaseDao() {
 
--- src/main/java/edu/shop/java/dao/impl/OrderFileDao.java
+++ src/main/java/edu/shop/java/dao/impl/OrderFileDao.java
@@ -1,14 +1,14 @@
 package edu.shop.java.dao.impl;
 
 
-import edu.shop.java.dao.OrderDAO;
+import edu.shop.java.dao.OrderDao;
 import edu.shop.java.models.Order;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository("orderFileDao")
-public class OrderFileDao extends FileAbstractDao<Order> implements OrderDAO{
+public class OrderFileDao extends FileAbstractDao<Order> implements OrderDao {
 
     public OrderFileDao() {
 
